0b6c1fbc4232f314af30b21e9d74af718f1bfbf5
Brian Davenport <briandav22@gmail.com>
2019-11-19 10:29:18 -0500

    adding styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import HomePage from './homepage.component'
+import HomePage from './pages/homepages/homepage.component'
+import './App.css'
 
 const App = () => {
   return (
